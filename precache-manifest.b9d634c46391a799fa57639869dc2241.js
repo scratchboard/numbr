5e0bb63cf2cbab16d035f383424d9297943c5a07
Omran Jamal <o.jamal97@gmail.com>
2020-05-31 20:48:39 +0600

    Working Tool

---
+++ precache-manifest.b9d634c46391a799fa57639869dc2241.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3172b82a63075cbd1722dddd25b0664",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83a602ff0b23e12cc1be",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "0579fab837655a0138f9",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "83a602ff0b23e12cc1be",
+    "url": "/static/js/2.4b68fbc9.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.4b68fbc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0579fab837655a0138f9",
+    "url": "/static/js/main.4f214741.chunk.js"
+  },
+  {
+    "revision": "90870a2e0324733a055e",
+    "url": "/static/js/runtime-main.96d2b16d.js"
+  }
+]);
